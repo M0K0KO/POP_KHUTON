6b174fb3dc10f6b085db8de2d9a34222701359b8
Hayoon Lim / 임하윤 <64838255+yoonyLim@users.noreply.github.com>
2025-05-09 05:26:16 +0900

    feat: dashboard nextjs

---
+++ POP_KHUTON/Assets/02Scripts/UIToolkit/ScriptUIToolkitTest.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ScriptUIToolkitTest : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        UIDocument uiDocument;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
